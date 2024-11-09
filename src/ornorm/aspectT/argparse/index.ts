0c298ed0cb7b5e059deaa07cd689626b18cf8d0b
abie <aime.biendo@wolterskluwer.com>
2024-11-09 19:06:04 +0100

    added correct file header

--- src/ornorm/aspectT/argparse/index.ts
+++ src/ornorm/aspectT/argparse/index.ts
@@ -1,1 +1,23 @@
+/**
+ * This TypeScript code is a port of the argparse library originally written in C.
+ * The original C code was created by Yecheng Fu <cofyc.jackson at gmail dot com>.
+ *
+ * Ported to TypeScript by Aimé Biendo <abiendo@gmail.com> as part of the AspectT Inc. AOP project.
+ *
+ * This file is part of the AspectT Inc. product, a seamless aspect-oriented extension to the TypeScript™ programming language.
+ * JavaScript platform compatible. Easy to learn and use.
+ *
+ * Copyright (C) 2012-2015 Yecheng Fu <cofyc.jackson at gmail dot com>
+ * All rights reserved.
+ *
+ * Licensed under the MIT License. You may obtain a copy of the License at
+ * https://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 export * from './h';
