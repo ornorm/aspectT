c53adbc35b7ec6a36eef9a76424ddc16c9f21061
abie <aime.biendo@wolterskluwer.com>
2024-11-09 17:38:56 +0100

    added log header

--- src/ornorm/aspectT/term/h.ts
+++ src/ornorm/aspectT/term/h.ts
@@ -6,57 +6,99 @@
  * in the LICENSE file.
  */
 
-export type ArgparseCallback = (self: Argparse, option: ArgparseOption) => number;
+export type argparse_callback = (self: argparse, option: argparse_option) => number;
 
-export enum ArgparseFlag {
-    STOP_AT_NON_OPTION = 1 << 0,
-    IGNORE_UNKNOWN_ARGS = 1 << 1,
+export enum argparse_flag {
+    argparse_STOP_AT_NON_OPTION = 1 << 0,
+    argparse_IGNORE_UNKNOWN_ARGS = 1 << 1,
 }
 
-export enum ArgparseOptionType {
-    END,
-    GROUP,
-    BOOLEAN,
-    BIT,
-    INTEGER,
-    FLOAT,
-    STRING,
+export enum argparse_option_type {
+    /* special */
+    argparse_OPT_END,
+    argparse_OPT_GROUP,
+    /* options with no arguments */
+    argparse_OPT_BOOLEAN,
+    argparse_OPT_BIT,
+    /* options with arguments (optional or required) */
+    argparse_OPT_INTEGER,
+    argparse_OPT_FLOAT,
+    argparse_OPT_STRING,
 }
 
-export enum ArgparseOptionFlags {
-    NONEG = 1,
+export enum argparse_option_flags {
+    /* disable negation */
+    OPT_NONEG = 1,
 }
 
-export type ArgparseOption = {
-    type: ArgparseOptionType;
-    shortName: string;
-    longName: string | null;
+/**
+ *  argparse option
+ *
+ *  `type`:
+ *    holds the type of the option, you must have an argparse_OPT_END last in your
+ *    array.
+ *
+ *  `short_name`:
+ *    the character to use as a short option name, '\0' if none.
+ *
+ *  `long_name`:
+ *    the long option name, without the leading dash, NULL if none.
+ *
+ *  `value`:
+ *    stores pointer to the value to be filled.
+ *
+ *  `help`:
+ *    the short help message associated to what the option does.
+ *    Must never be NULL (except for argparse_OPT_END).
+ *
+ *  `callback`:
+ *    function is called when corresponding argument is parsed.
+ *
+ *  `data`:
+ *    associated data. Callbacks can use it like they want.
+ *
+ *  `flags`:
+ *    option flags.
+ */
+export type argparse_option = {
+    type: argparse_option_type;
+    short_name: string;
+    long_name: string | null;
     value: any;
     help: string;
-    callback: ArgparseCallback | null;
+    callback: argparse_callback | null;
     data: any;
     flags: number;
 };
 
-export type Argparse = {
-    options: ArgparseOption[];
-    usages: string[];
+export type argparse = {
+    options: argparse_option[];
+    usages: Array<string>;
     flags: number;
+    // a description after usage
     description: string;
+    // a description at the end
     epilog: string;
+    // internal context
     argc: number;
-    argv: string[];
-    out: string[];
+    argv: Array<string>;
+    out: Array<string>;
     cpidx: number;
+    // current option value
     optvalue: string;
 };
 
+// built-in callbacks
+export type argparse_help_cb = (this: argparse, option: argparse_option) => number;
+
+export type argparse_help_cb_no_exit = (this: argparse, option: argparse_option) => number;
+
 // Built-in option macros
-export function createOptEnd(): ArgparseOption {
+export function OPT_END(): argparse_option {
     return {
-        type: ArgparseOptionType.END,
-        shortName: '',
-        longName: null,
+        type: argparse_option_type.argparse_OPT_END,
+        short_name: '',
+        long_name: null,
         value: null,
         help: '',
         callback: null,
@@ -65,11 +107,11 @@ export function createOptEnd(): ArgparseOption {
     };
 }
 
-export function createOptBoolean(shortName: string, longName: string | null, value: any, help: string, callback: ArgparseCallback | null, data: any, flags: number): ArgparseOption {
+export function OPT_BOOLEAN(short_name: string, long_name: string | null, value: any, help: string, callback: argparse_callback | null, data: any, flags: number): argparse_option {
     return {
-        type: ArgparseOptionType.BOOLEAN,
-        shortName,
-        longName,
+        type: argparse_option_type.argparse_OPT_BOOLEAN,
+        short_name,
+        long_name,
         value,
         help,
         callback,
@@ -78,11 +120,11 @@ export function createOptBoolean(shortName: string, longName: string | null, val
     };
 }
 
-export function createOptBit(shortName: string, longName: string | null, value: any, help: string, callback: ArgparseCallback | null, data: any, flags: number): ArgparseOption {
+export function OPT_BIT(short_name: string, long_name: string | null, value: any, help: string, callback: argparse_callback | null, data: any, flags: number): argparse_option {
     return {
-        type: ArgparseOptionType.BIT,
-        shortName,
-        longName,
+        type: argparse_option_type.argparse_OPT_BIT,
+        short_name,
+        long_name,
         value,
         help,
         callback,
@@ -91,11 +133,11 @@ export function createOptBit(shortName: string, longName: string | null, value: 
     };
 }
 
-export function createOptInteger(shortName: string, longName: string | null, value: any, help: string, callback: ArgparseCallback | null, data: any, flags: number): ArgparseOption {
+export function OPT_INTEGER(short_name: string, long_name: string | null, value: any, help: string, callback: argparse_callback | null, data: any, flags: number): argparse_option {
     return {
-        type: ArgparseOptionType.INTEGER,
-        shortName,
-        longName,
+        type: argparse_option_type.argparse_OPT_INTEGER,
+        short_name,
+        long_name,
         value,
         help,
         callback,
@@ -104,11 +146,11 @@ export function createOptInteger(shortName: string, longName: string | null, val
     };
 }
 
-export function createOptFloat(shortName: string, longName: string | null, value: any, help: string, callback: ArgparseCallback | null, data: any, flags: number): ArgparseOption {
+export function OPT_FLOAT(short_name: string, long_name: string | null, value: any, help: string, callback: argparse_callback | null, data: any, flags: number): argparse_option {
     return {
-        type: ArgparseOptionType.FLOAT,
-        shortName,
-        longName,
+        type: argparse_option_type.argparse_OPT_FLOAT,
+        short_name,
+        long_name,
         value,
         help,
         callback,
@@ -117,11 +159,11 @@ export function createOptFloat(shortName: string, longName: string | null, value
     };
 }
 
-export function createOptString(shortName: string, longName: string | null, value: any, help: string, callback: ArgparseCallback | null, data: any, flags: number): ArgparseOption {
+export function OPT_STRING(short_name: string, long_name: string | null, value: any, help: string, callback: argparse_callback | null, data: any, flags: number): argparse_option {
     return {
-        type: ArgparseOptionType.STRING,
-        shortName,
-        longName,
+        type: argparse_option_type.argparse_OPT_STRING,
+        short_name,
+        long_name,
         value,
         help,
         callback,
@@ -130,11 +172,11 @@ export function createOptString(shortName: string, longName: string | null, valu
     };
 }
 
-export function createOptGroup(help: string): ArgparseOption {
+export function OPT_GROUP(help: string): argparse_option {
     return {
-        type: ArgparseOptionType.GROUP,
-        shortName: '',
-        longName: null,
+        type: argparse_option_type.argparse_OPT_GROUP,
+        short_name: '',
+        long_name: null,
         value: null,
         help,
         callback: null,
@@ -143,12 +185,12 @@ export function createOptGroup(help: string): ArgparseOption {
     };
 }
 
-export function createOptHelp(): ArgparseOption {
-    return createOptBoolean('h', 'help', null, 'show this help message and exit', null, null, ArgparseOptionFlags.NONEG);
+export function OPT_HELP(): argparse_option {
+    return OPT_BOOLEAN('h', 'help', null, 'show this help message and exit', null, null, argparse_option_flags.OPT_NONEG);
 }
 
-// Function stubs (no implementation as per requirements)
-export function argparseInit(self: Argparse, options: ArgparseOption[], usages: string[], flags: number): void {}
-export function argparseDescribe(self: Argparse, description: string, epilog: string): void {}
-export function argparseParse(self: Argparse, argc: number, argv: string[]): void {}
-export function argparseUsage(self: Argparse): void {}
+// Function type aliases
+export type argparseInit = (this: argparse, options: Array<argparse_option>, usages: Array<string>, flags: number) => void;
+export type argparseDescribe = (this: argparse, description: string, epilog: string) => void;
+export type argparseParse = (this: argparse, argc: number, argv: Array<string>) => void;
+export type argparseUsage = (this: argparse) => void;
