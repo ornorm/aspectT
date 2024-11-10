5909827690113b24b6c7dc2e68ceccea5f5e1c77
abie <aime.biendo@wolterskluwer.com>
2024-11-10 21:42:51 +0100

    UPDATE fileObject licence

--- src/ornorm/aspectT/fs/file-object.ts
+++ src/ornorm/aspectT/fs/file-object.ts
@@ -1,42 +1,72 @@
+/**
+ * This TypeScript code is a port of the File class originally written in Java.
+ * The original Java code was created by Oracle and/or its affiliates.
+ *
+ * Ported to TypeScript by Aimé Biendo <abiendo@gmail.com> as part of the AspectT Inc. AOP project.
+ *
+ * This file is part of the AspectT Inc. product, a seamless aspect-oriented extension to the TypeScript™ programming language.
+ * JavaScript platform compatible. Easy to learn and use.
+ *
+ * Copyright (c) 1994, 2011, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
 import {FileFilter} from '@ornorm/aspectT';
 import {execSync} from 'child_process';
 import {EventEmitter} from 'events';
 import {
-    access,
     accessSync,
-    chmod,
     chmodSync,
     constants,
     createReadStream,
     createWriteStream,
     existsSync,
     FSWatcher,
-    MakeDirectoryOptions,
-    mkdir,
     mkdirSync,
     mkdtempSync,
     readdirSync,
     ReadStream,
-    realpath,
     realpathSync,
-    rename,
     renameSync,
-    stat,
     StatsBase,
     statSync,
-    unlink,
     unlinkSync,
-    utimes,
     utimesSync,
-    watch, WatchEventType,
-    writeFile,
+    watch,
+    WatchEventType,
     writeFileSync,
     WriteStream
 } from 'fs';
 import {tmpdir} from 'os';
 import {basename, delimiter, dirname, isAbsolute, join, resolve, sep} from 'path';
 
-export type  WatchHandler = (eventType: string, filename: string | null) => void;
+/**
+ * A type definition for a function that handles file system watch events.
+ *
+ * @param eventType - A string indicating the type of change (`"rename"` or `"change"`).
+ * @param filename - A string representing the name of the file that changed, or `null` if not provided.
+ */
+export type WatchHandler = (eventType: string, filename: string | null) => void;
 
 /**
  * A class that wraps around the `StatsBase` object to provide type-safe
